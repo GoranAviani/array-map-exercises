42048552a187801f687b3e0cebb229551b096b62
goranaviani <goranaviani@gmail.com>
2022-06-16 20:40:37 +0200

    w

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import first from './calcs/first'
+import {stringItUp} from "./calcs/stringItUp";
 
 
 function App() {
     console.log(first([2, 5, 100])); // [4, 10, 200]
+    console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
 
 
 
