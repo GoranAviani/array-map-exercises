c74e1f3571a2426f320cf75aa3c959dc849b1887
goranaviani <goranaviani@gmail.com>
2022-06-16 21:12:41 +0200

    exercise

--- src/App.js
+++ src/App.js
@@ -62,6 +62,35 @@ console.log(makeStrings([
 // "Bob Ziroll can go to The Matrix"]
 
 
+    console.log(readyToPutInTheDOM([
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]));
+// ["<h1>Angelina Jolie</h1><h2>80</h2>",
+// "<h1>Eric Jones</h1><h2>2</h2>",
+// "<h1>Paris Hilton</h1><h2>5</h2>",
+// "<h1>Kayne West</h1><h2>16</h2>",
+// "<h1>Bob Ziroll</h1><h2>100</h2>"]
+
+    
   return (
     <div className="App">
      app
@@ -114,6 +143,30 @@ console.log(makeStrings([
     }
 ])   }
 
+
+
+        {readyToPutInTheDOM([
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+])}
 
     </div>
   );
---
+++ src/calcs/readyToPutInTheDOM.js
@@ -0,0 +1,4 @@
+export const readyToPutInTheDOM = (arr) => {
+    const mapped = arr.map((x, k) => {<p><h1>{x.name}</h1> </p>})
+
+}
