239cee8ebaddbed8328e088fa31218a1c72eb5f4
goranaviani <goranaviani@gmail.com>
2022-06-16 20:45:43 +0200

    w

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import first from './calcs/first'
 import {stringItUp} from "./calcs/stringItUp";
-
+import {capitalizeNames} from "./calcs/capitalizeNames";
 
 function App() {
     console.log(first([2, 5, 100])); // [4, 10, 200]
     console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
+    console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
+
 
 
 
@@ -15,6 +17,7 @@ function App() {
      app
         {first([2, 5, 100])}
         {stringItUp([2, 5, 100])}
+        {capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])}
     </div>
   );
 }
