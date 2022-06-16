65079bc6e0c2dbdd0a647c966d3a44c9be61fcb6
goranaviani <goranaviani@gmail.com>
2022-06-16 21:07:31 +0200

    exercise

--- src/App.js
+++ src/App.js
@@ -90,6 +90,31 @@ console.log(makeStrings([
         age: 100
     }
 ])}
+
+        {makeStrings([
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+])   }
+
+
     </div>
   );
 }
