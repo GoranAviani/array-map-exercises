50baf8968bc218eff267d935035c0a5ac7102709
goranaviani <goranaviani@gmail.com>
2022-06-16 20:35:16 +0200

    array-map-exercises

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import first from 'calcs/first'
+
 
 function App() {
+    console.log(first([2, 5, 100])); // [4, 10, 200]
+
+
+
   return (
     <div className="App">
      app
