d3d4f358db7615e443361a57f31e27c07abba15e
goranaviani <goranaviani@gmail.com>
2022-06-16 20:50:53 +0200

    w

--- src/calcs/capitalizeNames.js
+++ src/calcs/capitalizeNames.js
@@ -1,5 +1,10 @@
+const capitalise = (word)=> {
+    const result = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    return result
+}
+
 export const capitalizeNames = (arr) => {
-    const mapped = arr.map((x,k) => <p key={k}>{x.autoCapitalize}</p>)
+    const mapped = arr.map((x,k) => <p key={k}>{capitalise(x)}</p>)
     return mapped
 
 }
