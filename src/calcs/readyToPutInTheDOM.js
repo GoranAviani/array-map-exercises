c6e9d368d49bbb85f14832324d1706c9777117a7
goranaviani <goranaviani@gmail.com>
2022-06-16 21:15:05 +0200

    exercise

--- src/calcs/readyToPutInTheDOM.js
+++ src/calcs/readyToPutInTheDOM.js
@@ -1,4 +1,4 @@
 export const readyToPutInTheDOM = (arr) => {
-    const mapped = arr.map((x, k) => {<p key={k}><h1>{x.name}</h1> <h2>{x.age}</h2></p>})
+    const mapped = arr.map((x, k) => {<p style={{color: "red"}} key={k}><h1>{x.name}</h1> <h2>{x.age}</h2></p>})
     return mapped
 }
