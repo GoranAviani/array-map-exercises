dd3582991acfd129bee11b689defbdb3d71ff987
goranaviani <goranaviani@gmail.com>
2022-06-16 21:07:28 +0200

    exercise

--- src/calcs/makeStrings.js
+++ src/calcs/makeStrings.js
@@ -1,10 +1,13 @@
 const checkAge = (obj) => {
     let result = []
     console.log(obj)
-
+    if(obj.age > 18){
+        result.push(obj.name + "is good")
+    }
+    return result
 }
 
 export const makeStrings = (arr) => {
-const result = arr.map((x, k) => <p>checkAge(x)</p>)
+const result = arr.map((x, k) => <p>{checkAge(x)}</p>)
     return result
 }
